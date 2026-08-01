2294bf6492ba7f0ba3e3b4e9f21195ccbc2cf178
jitendrabaghelugi <jitendra.baghel387944@gmail.com>
2026-08-01 19:53:25 +0530

    Update the Origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,5 @@
 import express from 'express'
-const app=express();
+const app = express();
 import cors from 'cors'
 import userRouter from './routes/userRoutes.js';
 import productRouter from './routes/productRoutes.js';
@@ -8,18 +8,21 @@ import orderRouter from './routes/orderRoutes.js'
 
 
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+    origin: [
+        "http://localhost:5173",
+        "https://mern-ecommerce-app-sepia.vercel.app",
+    ],
+    credentials: true
 }))
 
 app.use(express.json())
-app.use("/api/v1/user",userRouter)
-app.use("/api/v1/product",productRouter)
-app.use("/api/v1/cart",cartRouter)
-app.use("/api/v1/order",orderRouter)
+app.use("/api/v1/user", userRouter)
+app.use("/api/v1/product", productRouter)
+app.use("/api/v1/cart", cartRouter)
+app.use("/api/v1/order", orderRouter)
 
-app.get("/",(req,res)=>{
-    res.status(200).json({message:"Hello"})
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Hello" })
 })
 
 export default app
